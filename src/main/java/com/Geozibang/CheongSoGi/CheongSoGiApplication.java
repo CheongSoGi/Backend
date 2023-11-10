f8f73eb5061ae580950cbc0e38052f951846b242
siksik-Choi <wjdtlr4454@naver.com>
2023-11-10 17:37:44 +0900

    [ADD] Commit

---
+++ src/main/java/com/Geozibang/CheongSoGi/CheongSoGiApplication.java
@@ -0,0 +1,12 @@
+package com.Geozibang.CheongSoGi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheongSoGiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheongSoGiApplication.class, args);
+	}
+}
